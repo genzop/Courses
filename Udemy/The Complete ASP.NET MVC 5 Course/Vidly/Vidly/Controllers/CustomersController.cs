af52062e8ca53ef56828132bf8aa8df943c01e51
Enzo Panettieri <genzop@gmail.com>
2017-12-01 16:33:54 -0300

    Started The Complete ASP.NET MVC 5 Course

---
+++ Udemy/The Complete ASP.NET MVC 5 Course/Vidly/Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        [Route("Customers")]
+        public ActionResult Index()
+        {
+            var customers = new List<Customer>
+            {
+                new Customer { Id = 1, Name = "John Smith" },
+                new Customer { Id = 2, Name = "Mary Williams" }
+            };
+
+            return View(customers);
+        }
+
+        [Route("Customers/Details/{id}")]
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+    }
+}
