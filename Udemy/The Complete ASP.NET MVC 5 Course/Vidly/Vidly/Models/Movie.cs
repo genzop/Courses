cb021ece9ae55c00acb607e28344f2a513731e32
Enzo Panettieri <genzop@gmail.com>
2017-12-04 15:58:26 -0300

    Finished section 4 of The Complete ASP.NET MVC 5 Course

--- Udemy/The Complete ASP.NET MVC 5 Course/Vidly/Vidly/Models/Movie.cs
+++ Udemy/The Complete ASP.NET MVC 5 Course/Vidly/Vidly/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,12 +9,20 @@ namespace Vidly.Models
     public class Movie
     {
         public int Id { get; set; }
+
         public string Name { get; set; }
+        
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
+        
         public DateTime AddedDate { get; set; }
+
+        [Display(Name = "Number in Stock")]
         public int NumberInStock { get; set; }
 
+        [Display(Name = "Genre")]
         public byte GenreId { get; set; }
+
         public Genre Genre { get; set; }
     }
 }
