7b6ab0988fb88aa3300d8aec5387e15f83621bfd
Enzo Panettieri <genzop@gmail.com>
2018-04-20 13:01:21 -0300

    Finished section 6 of the Entity Framework in Depth: The Complete Guide course

---
+++ Udemy/Entity Framework/Queries/Queries/Models/PlutoContext.cs
@@ -0,0 +1,20 @@
+using Queries.Models.Configurations;
+using System.Data.Entity;
+
+
+namespace Queries.Models
+{
+    public class PlutoContext : DbContext
+    {
+        public PlutoContext() : base("name=PlutoContext") { }
+
+        public virtual DbSet<Author> Authors { get; set; }
+        public virtual DbSet<Course> Courses { get; set; }
+        public virtual DbSet<Tag> Tags { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new CourseConfiguration());
+        }
+    }
+}
---
+++ Udemy/Entity Framework/Queries/Vidzy/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using Vidzy.Models;
+
+namespace Vidzy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VidzyContext context = new VidzyContext();
+            var query1 = context.Videos.Where(x => x.Genre.Name == "Action").OrderBy(x => x.Name);
+            var query2 = context.Videos.Where(x => x.Genre.Name == "Drama" && x.Classification == Classification.Gold).OrderByDescending(x => x.ReleaseDate);
+            var query3 = context.Videos.Select(x => new { MovieName = x.Name, Genre = x.Genre });
+            var query4 = context.Videos.GroupBy(x => x.Classification).Select(x => new { Classification = x.Key, Movies = x });
+            var query5 = context.Videos.GroupBy(x => x.Classification).Select(x => new { Name = x.Key.ToString(), Count = x.Count() }).OrderBy(x => x.Name);
+            var query6 = context.Genres.Select(x => new { Name = x.Name, Count = x.Videos.Count }).OrderByDescending(x => x.Count);
+
+            foreach (var x in query6)
+            {
+                Console.WriteLine("{0} ({1})", x.Name, x.Count);
+            }
+        }
+    }
+}
