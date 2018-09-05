f322d4e1c9811685d41248ba37d108e86056be1f
Enzo Panettieri <genzop@gmail.com>
2018-09-05 15:04:54 -0300

    Finished section 3 of the ASP.NET Core Fundamentals course

--- Pluralsight/ASP.NET Core Fundamentals/OdeToFood/OdeToFood/Startup.cs
+++ Pluralsight/ASP.NET Core Fundamentals/OdeToFood/OdeToFood/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace OdeToFood
 {
@@ -18,17 +19,77 @@ namespace OdeToFood
             // Se registra la interface como un Singleton, es decir que solo va a haber una instancia de su implementacion durante la vida de la aplicacion.
             // Cada vez que alguien requiera un metodo que implemente IGreeter, se le injecta la clase Greeter.
             services.AddSingleton<IGreeter, Greeter>();
+
+            services.AddMvc();
         }
 
         // Define que componentes van a responder a cada mensaje HTTP que llega
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IGreeter greeter)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IGreeter greeter, ILogger<Startup> logger)
         {
+            #region ExceptionPage Middleware
+            // Si es ambiente de desarrollo
             if (env.IsDevelopment())
             {
+                // Registra un Middleware que intercepta todas las excepciones que ocurran durante la ejecucion del pipeline
                 app.UseDeveloperExceptionPage();
             }
+            #endregion
 
-            // Por cada request que llega, escribo en la respuesta un mensaje que contiene el texto "Hello World!"
+            #region DefaultFiles Middleware
+            // Establece como pagina por defecto a archivos con nombres estandard como index.html
+            /* app.UseDefaultFiles(); */
+            #endregion
+
+            #region StaticFiles Middleware
+            // Permite acceso a los archivos que se encuentran en la carpeta wwwroot
+            app.UseStaticFiles();
+            #endregion
+
+            #region FilesServer Middleware
+            // Cumple con la funcion de DefaultFiles y StaticFiles
+            /* app.UseFileServer(); */
+            #endregion
+
+            #region Custom Middleware
+            // Crea un middleware que utiliza la interface ILogger para escribir logs a la consola
+            /*
+            app.Use(next =>
+            {
+                return async context =>
+                {
+                    logger.LogInformation("Request incoming");
+
+                    // Si la ruta comienza con "/custom"
+                    if (context.Request.Path.StartsWithSegments("/custom"))
+                    {
+                        // Escribe en la respuesta un mensaje
+                        await context.Response.WriteAsync("Hit!!");
+                        logger.LogInformation("Request handled");
+                    }
+                    else
+                    {
+                        // Deja que el request siga por el pipeline
+                        await next(context);
+                        logger.LogInformation("Request outgoing");
+                    }
+                };
+            });
+            */
+            #endregion
+
+            #region UseWelcomePage Middleware
+            // Agrega el middleware UseWelcomePage al pipeline      
+            /*
+            app.UseWelcomePage(new WelcomePageOptions
+            {
+                Path = "/welcome"
+            });
+            */
+            #endregion
+
+            app.UseMvcWithDefaultRoute();
+
+            // Escribe en la respuesta un mensaje
             app.Run(async (context) =>
             {
                 var greeting = greeter.GetMessageOfTheDay();
