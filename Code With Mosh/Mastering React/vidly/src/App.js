ee711fee47a4f504294b9e07d694c2808d079214
Enzo Panettieri <genzop@gmail.com>
2019-03-07 00:06:09 -0300

    Finished section 6

--- Code With Mosh/Mastering React/vidly/src/App.js
+++ Code With Mosh/Mastering React/vidly/src/App.js
@@ -1,13 +1,30 @@
 import React, { Component } from "react";
-import "./App.css";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Movies from "./components/movies";
+import NavBar from "./components/navbar";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import MovieForm from "./components/movieForm";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <main className="container mt-4">
-        <Movies />
-      </main>
+      <React.Fragment>
+        <NavBar />
+        <main className="container mt-5">
+          <Switch>
+            <Route path="/movies/:id" component={MovieForm} />
+            <Route path="/movies" component={Movies} />
+            <Route path="/customers" component={Customers} />
+            <Route path="/rentals" component={Rentals} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect exact from="/" to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
     );
   }
 }
