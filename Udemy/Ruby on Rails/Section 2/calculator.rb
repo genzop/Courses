e87326826890344e20feee3b76f60980a7fc9715
Enzo Panettieri <genzop@gmail.com>
2019-05-31 08:21:04 -0300

    Section 2 - Part 10

---
+++ Udemy/Ruby on Rails/Section 2/calculator.rb
@@ -0,0 +1,12 @@
+puts "Simple calculator"
+25.times { print "-" }
+puts
+puts "Enter the first number"
+num_1 = gets.chomp
+puts "Enter the second number"
+num_2 = gets.chomp
+puts "The first number plus the second number is #{num_1.to_f + num_2.to_f}"
+puts "The first number minus the second number is #{num_1.to_f - num_2.to_f}"
+puts "The first number multiplied by the second number is #{num_1.to_f * num_2.to_f}"
+puts "The first number divided by the second number is #{num_1.to_f / num_2.to_f}"
+puts "The first number moduled by the second number is #{num_1.to_f % num_2.to_f}"
---
+++ Udemy/Ruby on Rails/Section 2/playground.rb
@@ -0,0 +1,10 @@
+puts "Hello World!"
+
+greeting = "Hello World 2!"
+puts greeting
+
+def say_hello(thing_to_say)
+    puts thing_to_say
+end
+
+say_hello.class
