3352f2f0d91debee048e3d181975d1601137110d
Enzo Panettieri <genzop@gmail.com>
2019-08-22 22:51:46 -0300

    Finished section 08

---
+++ Udemy/Vue.js 2/Section 08/first-app/src/components/User.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="component">
+        <h1>The User Component</h1>
+        <p>I'm an awesome User!</p>
+        <button @click="changeName">Change my Name</button>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <app-user-detail :name="name" @nameWasReset="name = $event.name" :resetFn="resetName" :userAge="age"></app-user-detail>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <app-user-edit :userAge="age" :editFn="editAge"></app-user-edit>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import UserDetail from './UserDetail.vue';
+    import UserEdit from './UserEdit.vue';
+
+    export default {
+        data: function() {
+            return {
+                name: 'Enzo',
+                age: 24
+            }
+        },
+        methods: {
+            changeName() {
+                this.name = 'Gino';
+            },
+            resetName() {
+                this.name = 'Gino';
+            },
+            editAge() {
+                this.age = 30;
+            }
+        },
+        components: {
+            appUserDetail: UserDetail,
+            appUserEdit: UserEdit
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightblue;
+    }
+</style>
