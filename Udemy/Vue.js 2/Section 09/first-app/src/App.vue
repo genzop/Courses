eb74a3833218ec13f775e4d2e33fcd1f612618ad
Enzo Panettieri <genzop@gmail.com>
2019-08-23 17:22:02 -0300

    Finished section 09

---
+++ Udemy/Vue.js 2/Section 09/first-app/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">              
+                <app-quote>
+                    <h2 slot="title">{{ quoteTitle }}</h2>
+                    <p>A wonderful Quote</p>                    
+                </app-quote>
+
+                <hr>
+                <button @click="selectedComponent = 'app-quote'">Quote</button>
+                <button @click="selectedComponent = 'app-author'">Author</button>
+                <button @click="selectedComponent = 'app-new'">New</button>                
+                <p>{{ selectedComponent }}</p>
+                <keep-alive>
+                    <component :is="selectedComponent">
+                        <p>Default Content</p>
+                    </component>
+                </keep-alive>                
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Quote from './components/Quote.vue';
+    import Author from './components/Author.vue';
+    import New from './components/New.vue';
+
+    export default {
+        data: function() {
+            return {
+                quoteTitle: 'The Quote',
+                selectedComponent: 'app-quote'
+            }
+        },
+        components: {
+            'app-quote': Quote,
+            'app-author': Author,
+            'app-new': New
+        }
+    }
+</script>
+
+<style>
+</style>
